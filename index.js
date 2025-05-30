f5393a999076c3b72069f02e26d6ef93f4b770db
kimp0405 <kevin.isrrael.molina@gmail.com>
2025-05-30 03:33:12 -0400

    Primer Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function generateExcuse() {
+  const whoRandom = who[Math.floor(Math.random() * who.length)];
+  const actionRandom = action[Math.floor(Math.random() * action.length)];
+  const whatRandom = what[Math.floor(Math.random() * what.length)];
+  const whenRandom = when[Math.floor(Math.random() * when.length)];
+  return `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}.`;
+}
+let p = document.querySelector('#excuse');
+p.innerHTML = generateExcuse();
+console.log(generateExcuse());
